c56c3a5a6af1f479e0850435d88cf85d5231c336
venkat <79528964+venkateshwarbabu@users.noreply.github.com>
2024-12-06 15:41:43 +0530

    Update IrctcminiApplication.java

--- src/main/java/com/irctc/IrctcminiApplication.java
+++ src/main/java/com/irctc/IrctcminiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class IrctcminiApplication {
 
 	public static void main(String[] args) {
